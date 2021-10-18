8b6354994fe2142eb18422bc1ee3d79d4c770d2c
mngduyphuong <mngduyphuong@gmail.com>
2021-10-18 09:58:16 +0700

    test build jenkins

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>Testing add new content to deploy</p>
   </div>
 </template>
 
