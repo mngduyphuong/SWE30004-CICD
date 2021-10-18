988af5051860d9af250d36d82ba175fc1022688d
mngduyphuong <mngduyphuong@gmail.com>
2021-10-18 21:35:58 +0700

    test

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <p>Testing auto deploy to github pages version 2</p>
+    <p>Testing auto deploy to github pages version 3</p>
   </div>
 </template>
 
